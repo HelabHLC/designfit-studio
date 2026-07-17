870faf06aa8ab4e058b0078c26de7dd55602ac70
HelabHLC <norbert.m.woiwod@gmail.com>
2026-07-17 18:21:51 +0200

    export industrial decision report

--- src/core/industrial-decision/index.ts
+++ src/core/industrial-decision/index.ts
@@ -2,3 +2,4 @@ export * from "./industrial-decision-contract";
 export * from "./industrial-decision-engine";
 export * from "./decision-graph";
 export * from "./decision-ledger";
+export * from "./industrial-decision-report";
